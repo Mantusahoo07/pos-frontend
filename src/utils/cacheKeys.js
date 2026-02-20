845221e892e3ddd4d992672a3c1139ce48fbf922
Mantusahoo07 <mantusahoo.bgr@gmail.com>
2026-02-20 12:40:14 +0530

    Update cacheKeys.js

--- src/utils/cacheKeys.js
+++ src/utils/cacheKeys.js
@@ -2,8 +2,6 @@ export const QUERY_KEYS = {
   ORDERS: 'orders',
   ORDER: 'order',
   TABLES: 'tables',
-  AVAILABLE_TABLES: 'available-tables',
   USER: 'user',
   METRICS: 'metrics',
-  POPULAR_DISHES: 'popular-dishes',
 };
