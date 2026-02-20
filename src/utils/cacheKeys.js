38349b2d3fdbcd55ef940dcfa4ef855995862e90
Mantusahoo07 <mantusahoo.bgr@gmail.com>
2026-02-20 12:34:11 +0530

    Add cacheKeys.js with query key constants

---
+++ src/utils/cacheKeys.js
@@ -0,0 +1,9 @@
+export const QUERY_KEYS = {
+  ORDERS: 'orders',
+  ORDER: 'order',
+  TABLES: 'tables',
+  AVAILABLE_TABLES: 'available-tables',
+  USER: 'user',
+  METRICS: 'metrics',
+  POPULAR_DISHES: 'popular-dishes',
+};
