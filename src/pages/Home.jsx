b5c2c00ead89fa6c60af07d44aa961ac32ddb3ef
Mantusahoo07 <mantusahoo.bgr@gmail.com>
2026-02-20 09:08:43 +0530

    Refactor Home component layout structure
    Refactor section element for better structure.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,8 +14,8 @@ const Home = () => {
     }, [])
 
   return (
-    <section className="bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3 pb-24">      {/* Left Div */}
-      <div className="flex-[3]">
+    <section className="bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden pb-24">
+        <div className="flex-[3]">
         <Greetings />
         <div className="flex items-center w-full gap-3 px-8 mt-8">
           <MiniCard title="Total Earnings" icon={<BsCashCoin />} number={512} footerNum={1.6} />
